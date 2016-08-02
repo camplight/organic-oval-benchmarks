a381ec03aaf56f8be11057786faaf7aa91bf65e4
outbounder <obiwonn@gmail.com>
2016-08-02 09:53:58 +0300

    do not render error 404 and 500 templates

--- server/http/responders/default.js
+++ server/http/responders/default.js
@@ -2,20 +2,12 @@ module.exports = function (app, dna) {
   // default not found handler
   app.use(function (req, res, next) {
     res.status(404)
-    if (dna.views) {
-      res.render('404')
-    } else {
-      res.end()
-    }
+    res.end()
   })
   // default error handler
   app.use(function (err, req, res, next) {
     console.error('HTTP', err.stack)
     res.status(500)
-    if (dna.views) {
-      res.render('500')
-    } else {
-      res.end()
-    }
+    res.end()
   })
 }
